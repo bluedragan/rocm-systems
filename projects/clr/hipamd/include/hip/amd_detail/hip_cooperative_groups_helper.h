5ac12714d80b170d84bb8ee1e1b4d63badfd09e5
Jatin Chaudhary <jatchaud@amd.com>
2025-07-01 14:21:39 +0000

    SWDEV-493792 - add split barriers for grid_group

--- projects/clr/hipamd/include/hip/amd_detail/hip_cooperative_groups_helper.h
+++ projects/clr/hipamd/include/hip/amd_detail/hip_cooperative_groups_helper.h
@@ -191,6 +191,9 @@ __CG_STATIC_QUALIFIER__ dim3 grid_dim() {
                static_cast<__hip_uint32_t>(gridDim.z)));
 }
 
+__CG_STATIC_QUALIFIER__ unsigned int barrier_signal() { return __ockl_grid_bar_arrive(); }
+
+__CG_STATIC_QUALIFIER__ void barrier_wait(unsigned int s) { __ockl_grid_bar_wait(s); }
 }  // namespace grid
 
 /**
@@ -227,7 +230,6 @@ __CG_STATIC_QUALIFIER__ dim3 block_dim() {
   return (dim3(static_cast<__hip_uint32_t>(blockDim.x), static_cast<__hip_uint32_t>(blockDim.y),
                static_cast<__hip_uint32_t>(blockDim.z)));
 }
-
 }  // namespace workgroup
 
 namespace tiled_group {
