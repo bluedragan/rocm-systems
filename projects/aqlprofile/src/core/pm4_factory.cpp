f30dce66aacfdc6879bba9f00e4171cc27e7bfa4
Mythreya <mythreya.kuricheti@amd.com>
2025-09-26 23:08:19 +0000

    code review

--- projects/aqlprofile/src/core/pm4_factory.cpp
+++ projects/aqlprofile/src/core/pm4_factory.cpp
@@ -21,10 +21,13 @@
 // THE SOFTWARE.
 
 #include "pm4_factory.h"
+#include "logger.h"
 
 #include <cstring>
 #include <mutex>
 #include <shared_mutex>
+#include <stdexcept>
+#include <string_view>
 
 namespace aql_profile {
 namespace {
@@ -58,16 +61,29 @@ aqlprofile_agent_handle_t RegisterAgent(const aqlprofile_agent_info_v1_t* agent_
   // Update size to new size
   static_assert(sizeof(aqlprofile_agent_info_v1_t) == 40);
 
-  aqlprofile_agent_handle_t agent_id {};
-  AgentInfo int_agent_info {};
-  int_agent_info.cu_num = agent_info->cu_num;
-  int_agent_info.se_num = agent_info->se_num;
-  int_agent_info.xcc_num = agent_info->xcc_num;
-  int_agent_info.shader_arrays_per_se = agent_info->shader_arrays_per_se;
-  int_agent_info.domain = agent_info->domain;
-  int_agent_info.bdf_id = agent_info->location_id;
+  aqlprofile_agent_handle_t agent_id{};
+  AgentInfo int_agent_info{};
+
+  if (agent_info->size == 40) {
+    int_agent_info.cu_num = agent_info->cu_num;
+    int_agent_info.se_num = agent_info->se_num;
+    int_agent_info.xcc_num = agent_info->xcc_num;
+    int_agent_info.shader_arrays_per_se = agent_info->shader_arrays_per_se;
+    int_agent_info.domain = agent_info->domain;
+    int_agent_info.bdf_id = agent_info->location_id;
+  } else {
+    ERR_LOGGING << "Unsupported agent_info size " << agent_info->size;
+    throw std::runtime_error("Unsupported agent_info size");
+  }
 
   static_assert(sizeof(int_agent_info.name) == 64);
+
+  if (agent_info->agent_gfxip == nullptr) {
+    constexpr auto name_null =
+        std::string_view{"Agent name 'agent_info.agent_gfxip' cannot be null"};
+    ERR_LOGGING << name_null;
+    throw std::runtime_error(name_null.data());
+  }
 
   auto len = strlen(agent_info->agent_gfxip);
   memset(int_agent_info.name, 0, sizeof(int_agent_info.name));
