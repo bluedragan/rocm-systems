896d73397e3409a72d9037e3012ca8a585702965
Mark Meserve <Mark.Meserve@amd.com>
2025-11-12 01:09:55 +0000

    attach: rocprof-compute ROCAttach updates
    - Update to new library names
    - Correct usage of C lib detach

--- projects/rocprofiler-compute/src/rocprof_compute_profile/profiler_rocprofiler_sdk.py
+++ projects/rocprofiler-compute/src/rocprof_compute_profile/profiler_rocprofiler_sdk.py
@@ -92,7 +92,8 @@ class rocprofiler_sdk_profiler(RocProfCompute_Base):
                 / "librocprofiler-sdk-rocattach.so"
             )
             options.update({
-                "ROCPROF_ATTACH_TOOL_LIBRARY": rocprofiler_attach_tool_path,
+                "ROCPROF_ATTACH_TOOL_LIBRARY": rocprofiler_sdk_tool_path
+                "ROCPROF_ATTACH_LIBRARY": rocprofiler_attach_library_path,
                 "ROCPROF_ATTACH_PID": args.attach_pid,
             })
 
