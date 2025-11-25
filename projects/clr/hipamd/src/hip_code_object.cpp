8676ac365f8a28e029c48dadf1f14715490dad65
Gerardo Hernandez <gerardo.hernandez@amd.com>
2025-11-25 14:18:44 +0000

    SWDEV-508225 - do not assert() after calling digestFatBinary() if it fails. Otherwise this causes assertions to trigger easily in systems that have an APU and a discrete GPU and the code was compiled for the discrete one

--- projects/clr/hipamd/src/hip_code_object.cpp
+++ projects/clr/hipamd/src/hip_code_object.cpp
@@ -282,7 +282,6 @@ FatBinaryInfo** StatCO::addFatBinary(const void* data, bool initialized, bool& s
   }
 
   hipError_t err = digestFatBinary(data, modules_[data]);
-
   success = (err == hipSuccess);
   return &modules_[data];
 }
@@ -453,8 +452,7 @@ hipError_t StatCO::getStatFunc(hipFunction_t* hfunc, const void* hostFunction, i
   if (*(module) == nullptr) {
     amd::ScopedLock lock(sclock_);
     if (*(module) == nullptr) {
-      hipError_t err = digestFatBinary(module_to_hostModule_[module], *module);
-      assert(err == hipSuccess);
+      std::ignore = digestFatBinary(module_to_hostModule_[module], *module);
     }
   }
 
@@ -473,8 +471,7 @@ hipError_t StatCO::getStatFuncAttr(hipFuncAttributes* func_attr, const void* hos
   // Lazy load
   FatBinaryInfo** module = it->second->moduleInfo();
   if (*(module) == nullptr) {
-    hipError_t err = digestFatBinary(module_to_hostModule_[module], *module);
-    assert(err == hipSuccess);
+    std::ignore = digestFatBinary(module_to_hostModule_[module], *module);
   }
 
   return it->second->getStatFuncAttr(func_attr, deviceId);
@@ -505,8 +502,7 @@ hipError_t StatCO::getStatGlobalVar(const void* hostVar, int deviceId, hipDevice
   // Lazy load
   FatBinaryInfo** module = it->second->moduleInfo();
   if (*(module) == nullptr) {
-    hipError_t err = digestFatBinary(module_to_hostModule_[module], *module);
-    assert(err == hipSuccess);
+    std::ignore = digestFatBinary(module_to_hostModule_[module], *module);
   }
 
   DeviceVar* dvar = nullptr;
@@ -532,8 +528,7 @@ hipError_t StatCO::initStatManagedVarDevicePtr(int deviceId) {
         // Lazy load
         FatBinaryInfo** module = var->moduleInfo();
         if (*(module) == nullptr) {
-          err = digestFatBinary(module_to_hostModule_[module], *module);
-          assert(err == hipSuccess);
+          std::ignore = digestFatBinary(module_to_hostModule_[module], *module);
         }
         hip::Stream* stream = g_devices.at(deviceId)->NullStream();
         if (stream == nullptr) {
