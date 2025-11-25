a644931b42d76b2f3d50edfbd3eb160554ee4ea8
Abhinab Paul Choudhury <abchoudh@amd.com>
2025-11-25 04:52:27 -0600

    Unnest profile method

--- projects/rocprofiler-compute/src/rocprof_compute_profile/profiler_base.py
+++ projects/rocprofiler-compute/src/rocprof_compute_profile/profiler_base.py
@@ -391,6 +391,62 @@ class RocProfCompute_Base:
             soc=self._soc,
         )
 
+    def profile(
+        self,
+        fnames: Union[list[Path], Path],
+        options: Union[list[str], dict[str, Any]],
+        total_runs: int = 1,
+    ) -> float:
+        args = self.get_args()
+
+        if isinstance(fnames, list):
+            console_log(
+                "profiling", f"Current input files: {', '.join(map(str, fnames))}"
+            )
+            str_fnames = [str(fname) for fname in fnames]
+        else:
+            console_log("profiling", f"Current input file: {fnames}")
+            str_fnames = str(fnames)
+
+        start_time = time.time()
+
+        if self.__profiler == "rocprofv3" or self.__profiler == "rocprofiler-sdk":
+            # Only 1-run case is permitted for attach/detach
+            if (isinstance(options, list) and "--pid" in options) or (
+                isinstance(options, dict)
+                and (options.get("ROCPROF_ATTACH_PID") is not None)
+            ):
+                if total_runs > 1:
+                    console_error(
+                        f"Cannot attach process for profiling as the requested "
+                        f"performance counters exceed the collection capacity of "
+                        f"single pass counter collection. The current setup of "
+                        f"requested counter blocks needs {total_runs} number of "
+                        f'passes. Please use "--block" or "--set" '
+                        f"to adjust or reduce the requested performance metrics!"
+                    )
+            run_prof(
+                fnames=str_fnames,
+                profiler_options=options,
+                workload_dir=args.path,
+                mspec=self._soc._mspec,
+                loglevel=args.loglevel,
+                format_rocprof_output=args.format_rocprof_output,
+                retain_rocpd_output=args.retain_rocpd_output,
+            )
+
+            end_time = time.time()
+            duration = end_time - start_time
+
+            console_debug(
+                f"The time of run_prof of {str_fnames} is {int(duration / 60)} min"
+                f" {duration % 60} sec"
+            )
+            return duration
+        else:
+            console_error("Profiler not supported")
+            return 0.0
+
     @abstractmethod
     def run_profiling(self, version: str, prog: str) -> None:
         """Run profiling."""
@@ -509,57 +565,6 @@ class RocProfCompute_Base:
                 else:
                     console_debug(output)
 
-        def profile(
-            fnames: Union[list[Path], Path], options: Union[list[str], dict[str, Any]]
-        ) -> float:
-            if isinstance(fnames, list):
-                console_log(
-                    "profiling", f"Current input files: {', '.join(map(str, fnames))}"
-                )
-                str_fnames = [str(fname) for fname in fnames]
-            else:
-                console_log("profiling", f"Current input file: {fnames}")
-                str_fnames = str(fnames)
-
-            start_time = time.time()
-
-            if self.__profiler == "rocprofv3" or self.__profiler == "rocprofiler-sdk":
-                # Only 1-run case is permitted for attach/detach
-                if (isinstance(options, list) and "--pid" in options) or (
-                    isinstance(options, dict)
-                    and (options.get("ROCPROF_ATTACH_PID") is not None)
-                ):
-                    if total_runs > 1:
-                        console_error(
-                            f"Cannot attach process for profiling as the requested "
-                            f"performance counters exceed the collection capacity of "
-                            f"single pass counter collection. The current setup of "
-                            f"requested counter blocks needs {total_runs} number of "
-                            f'passes. Please use "--block" or "--set" '
-                            f"to adjust or reduce the requested performance metrics!"
-                        )
-                run_prof(
-                    fnames=str_fnames,
-                    profiler_options=options,
-                    workload_dir=args.path,
-                    mspec=self._soc._mspec,
-                    loglevel=args.loglevel,
-                    format_rocprof_output=args.format_rocprof_output,
-                    retain_rocpd_output=args.retain_rocpd_output,
-                )
-
-                end_time = time.time()
-                duration = end_time - start_time
-
-                console_debug(
-                    f"The time of run_prof of {fname} is {int(duration / 60)} min"
-                    f" {duration % 60} sec"
-                )
-                return duration
-            else:
-                console_error("Profiler not supported")
-                return 0.0
-
         if args.iteration_multiplexing is not None:
             console_log(
                 "profiling", f"Iteration multiplexing: {args.iteration_multiplexing}"
@@ -582,7 +587,7 @@ class RocProfCompute_Base:
                     ),
                 )
 
-            profile(input_files, options)
+            self.profile(input_files, options)
         else:
             console_log("profiling", "Iteration multiplexing: Disabled")
 
@@ -608,7 +613,7 @@ class RocProfCompute_Base:
                         "pending first measurement...]"
                     )
 
-                duration = profile(fname, options)
+                duration = self.profile(fname, options, total_runs)
                 total_profiling_time += duration
 
         # Delete temporary native tool if created
