b826a04d429b7a4c491474c5db3c07bc293e7371
Ioannis Assiouras <Ioannis.Assiouras@amd.com>
2025-11-28 03:30:04 +0000

    Fix Unit_hipStreamBeginCaptureToGraph_CapturePartialInThreads

--- projects/clr/hipamd/src/hip_event.cpp
+++ projects/clr/hipamd/src/hip_event.cpp
@@ -396,6 +396,9 @@ hipError_t hipEventRecord_common(hipEvent_t event, hipStream_t stream, unsigned 
   hip::Event* e = reinterpret_cast<hip::Event*>(event);
   hip::Stream* s = reinterpret_cast<hip::Stream*>(stream);
   hip::Stream* hip_stream = hip::getStream(stream);
+  if (hipStream_t lastCaptureStream = e->GetCaptureStream()) {
+    reinterpret_cast<hip::Stream*>(lastCaptureStream)->EraseCaptureEvent(event); 
+  }
   e->SetCaptureStream(stream);
   if ((stream != nullptr && stream != hipStreamLegacy) &&
       (s->GetCaptureStatus() == hipStreamCaptureStatusActive)) {
